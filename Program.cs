b83eb3a66e76d9844180c8b5b0692a7af3a757bf
Murilo Iachitzke <miachitzke@mail.com>
2024-04-26 14:22:25 -0300

    finalização do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Instancia de teste do Nokia
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "97478716", modelo: "Tijolão", imei: 111111111, memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+Console.WriteLine("\n");
+
+// Instancia de teste do iPhone
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "707070", modelo: "XR", imei: 302329252, memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
